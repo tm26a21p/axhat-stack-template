6c8e6e09277de6ec0f5a6d5dab2c514fe18bc841
tm26a21p <louis.piochaud@epitech.eu>
2024-05-19 12:07:03 -0700

    feat (main.rs): added log message

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,9 @@ async fn main()
         .nest("/public", serve_static_files());
 
     let listener =
-        tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+        tokio::net::TcpListener::bind("0.0.0.0:3000").await.expect("Failed to bind port. port 3000 Already in use?");
+    println!("Server running on: http://localhost:3000");
+        
     axum::serve(listener, app).await.unwrap();
 }
 
