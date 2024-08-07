66ac65d1aff4c49af4448fa6a06bb1af0eff9253
tm26a21p <louis.piochaud@epitech.eu>
2024-08-07 14:58:20 +0200

    fix: making port and address variables, making the code a little cleaner

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,22 @@ use axum::{routing::get, Router};
 use crate::routes::*;
 mod routes;
 mod templates;
-mod utils;
 
 #[tokio::main]
 async fn main()
 {
     tracing_subscriber::fmt::init();
+    let address = "0.0.0.0";
+    let port = "4004";
     let app = Router::new()
         .route("/", get(index))
         .route("/more-content", get(more_content))
         .nest("/public", routes::serve_static_files());
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let listener = tokio::net::TcpListener::bind(address.to_owned() + ":" + &port)
         .await
-        .expect("Failed to bind port. port 3000 Already in use?");
-    println!("Server running on: http://localhost:3000");
+        .expect("Failed to bind port.");
+    println!("Server running on http://{}:{}", "localhost", port);
 
     axum::serve(listener, app).await.unwrap();
 }
